db3a46fbbda791c6050260f0e2cbd0d7019509b0
marion-randrianasolo <marionlaury.r@gmail.com>
2024-01-17 09:55:10 +0100

    titanic main

--- Titanic/src/main/java/Titanic.java
+++ Titanic/src/main/java/Titanic.java
@@ -1,9 +1,27 @@
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
 
 public class Titanic {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		SparkSession sparkSession  = SparkSession.builder().appName("Titanic Data").master("local[3]").getOrCreate();
+		sparkSession.sparkContext().setLogLevel("WARN");
+		
+		String dataFile = "/home/marionlaury/Téléchargements/train.csv";
+		
+		Dataset<Row> df = sparkSession.read()
+				.format("csv")
+				.option("header", "true")
+				.option("delimiter", ",")
+				.load(dataFile);
+		
+		df.show();
+		
+	
+		//System.out.println("?pmbre de l")
+		
+		df.describe().show();
 	}
 
 }
